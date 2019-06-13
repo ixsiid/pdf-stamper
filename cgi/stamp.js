c8adbdf0136aab6b003dd836de8ad0ff7c102557
ixsiid <ixsiid@halzion.net>
2019-06-13 13:33:57 +0900

    stamp by python script

--- cgi/stamp.js
+++ cgi/stamp.js
@@ -2,13 +2,18 @@ const version = require('../version.json').datetime;
 const childProcess = require('child_process');
 
 module.exports = function (req, res) {
-	const approved = childProcess.execSync(`python ./cgi/add_text.py ${req.body.author}`, {
+	const approved = childProcess.execSync(`python ./cgi/add_text.py "${req.body.author}"`, {
 		input: Buffer.from(req.body.pdf['$content'], 'base64'),
+		stdio: ['pipe', 'pipe', 'inherit'],
+		maxBuffer: 50 * 1024 * 1024,
 	});
 
 	const result = { version };
 	result.success = true;
-	result.approved = approved;
+	result.approved = {
+		"$content-type": "application/pdf",
+		"$content": approved
+	};
 	result.message = `Your name is ${req.body.author}`;
 	res.json(result);
 };
